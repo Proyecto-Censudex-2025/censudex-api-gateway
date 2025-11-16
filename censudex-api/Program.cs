1f385935ad8e8086eda7ee967dc3e11a51545cc4
towcrash <fernando.vega@alumnos.ucn.cl>
2025-11-16 04:21:20 -0300

    Update program.cs

--- censudex-api/Program.cs
+++ censudex-api/Program.cs
@@ -5,13 +5,58 @@ using OrdersService.Grpc;
 using Polly;
 using Polly.Extensions.Http;
 using Serilog;
+using ClientsService.Grcp;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApi();
+builder.Services.AddMemoryCache();
 
 builder.Host.UseSerilog((ctx, lc) => lc.WriteTo.Console());
+
+// Add JWT Authentication
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+            ClockSkew = TimeSpan.Zero,
+            RoleClaimType = "http://schemas.microsoft.com/ws/2008/06/identity/claims/role"
+        };
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AdminOnly", policy =>
+        policy.RequireRole("Admin"));
+
+    options.AddPolicy("ClientOrAbove", policy =>
+        policy.RequireRole("Admin", "User"));
+});
+
+builder.Services.AddHttpClient("AuthService", client =>
+{
+    client.BaseAddress = new Uri(builder.Configuration["Services:AuthService:BaseUrl"]);
+    client.Timeout = TimeSpan.FromSeconds(30);
+});
+
+builder.Services.AddGrpcClient<ClientService.ClientServiceClient>(o =>
+{
+    o.Address = new Uri(builder.Configuration["Services:GrpcBalancer"]);
+});
 
 builder.Services.AddGrpcClient<Inventory.InventoryClient>(o =>
 {
@@ -29,7 +74,17 @@ builder.Services.AddGrpcClient<OrdersService.Grpc.OrdersService.OrdersServiceCli
     o.Address = new Uri(builder.Configuration["Services:GrpcBalancer"]);
 });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
+builder.Services.AddScoped<ClientsGrpcAdapter>();
 builder.Services.AddScoped<InventoryGrpcAdapter>();
 builder.Services.AddScoped<ProductsGrpcAdapter>();
 builder.Services.AddScoped<OrdersGrpcAdapter>();
@@ -44,6 +99,10 @@ if (app.Environment.IsDevelopment())
 }
 
 
+app.UseHttpsRedirection();
+app.UseCors("AllowAll");
+app.UseMiddleware<censudex_api.src.Middleware.TokenValidationMiddleware>();
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
